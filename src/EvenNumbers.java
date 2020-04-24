11595535a97c3e5fec32b66714451a56643d1324
Igor Trochimiuk <3491298@gmail.com>
2020-04-24 14:44:57 +0200

    Zad 6.3 - liczby parzyste

---
+++ src/EvenNumbers.java
@@ -0,0 +1,12 @@
+public class EvenNumbers {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+                suma += i;
+            }
+        }
+        System.out.println(suma);
+    }
+}
